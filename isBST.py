dd1c8e73e509727150eaad1ff32754870dfc2d6c
moonlimb <moonlimb@gmail.com>
2013-02-01 00:25:10 -0800

    improved code for isBST.py

---
+++ isBST.py
@@ -0,0 +1,12 @@
+from Node import Node
+
+def is_BST(node):
+    """returns True if a given node is a root node of a binary search tree"""
+    if node.is_leaf():
+        return True
+    else:
+        # Node class contains comparison methods
+        if (node.left and node.left >= node) || (node.right and node >= node.right):
+            return False 
+
+    return isBST(node.left) and isBST(node.right)
